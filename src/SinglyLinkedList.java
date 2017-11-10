ffa71803b33a6ca58e9bae0be47869f04565a351
AGonz123 <32293916+AGonz123@users.noreply.github.com>
2017-11-09 16:41:51 -0800

    Update SinglyLinkedList.java

--- src/SinglyLinkedList.java
+++ src/SinglyLinkedList.java
@@ -1,17 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author Andrew Gonzales
- */
-/**
- * Implements the ListInterface
- * using singly linked list
- */
 public class SinglyLinkedList<T> implements ListInterface<T> {
 
     protected Node<T> first;
