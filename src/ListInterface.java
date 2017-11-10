3c17db3472a4ab30586a65e339034e0787fad268
AGonz123 <32293916+AGonz123@users.noreply.github.com>
2017-11-09 16:42:51 -0800

    Update ListInterface.java

--- src/ListInterface.java
+++ src/ListInterface.java
@@ -1,15 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author Andrew Gonzales
- */
-
-  
  //*An interface for the ADT list.
  //@author Andrew Gonzales
  //@version 1.0
